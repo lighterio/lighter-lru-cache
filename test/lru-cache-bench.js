1ef0756f1bc993ac8ae4a007653b047e560044ab
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-17 08:24:03 -0700

    Initial commit

---
+++ test/lru-cache-bench.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var lru-cache = require(__dirname + '/lighter-lru-cache')
+
+describe('Benchmarks', function () {
+
+  bench('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
---
+++ test/lru-cache-test.js
@@ -0,0 +1,26 @@
+var lru-cache = require('../lighter-lru-cache')
+var is = global.is || require('exam/lib/is')
+
+describe('lru-cache', function () {
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
